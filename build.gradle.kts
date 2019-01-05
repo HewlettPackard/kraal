9418f2a0b30d038b9952e37d5de6ac6cb5a49b2d
Brad Newman <brad.newman@hpe.com>
2019-01-04 17:37:25 -0800

    Update version for 0.0.4 development

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ description = "Kraal parent module - Kraal enables the use of Kotlin coroutines 
 
 allprojects {
     group = "com.hpe.kraal"
-    version = "0.0.3"
+    version = "0.0.4"
 
     repositories {
         jcenter()
